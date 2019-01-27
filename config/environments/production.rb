64a99adcbb5e1f236001655b4a82d7d674988515
basistamarcin <basistamarcin@gmail.com>
2019-01-27 08:26:30 -0500

    dodane devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
+  config.action_mailer.default_url_options = { host: 'https://ruby-project-basistamarcin623044.codeanyapp.com', port: 3000 }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
