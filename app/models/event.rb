8ff200e1ea3ae856a0e9fd6af48ab2bbb9f433c2
basistamarcin <basistamarcin@gmail.com>
2019-01-27 08:00:59 -0500

    pelna oblsuga eventow w ticketach

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,10 @@ class Event < ApplicationRecord
   validates :price_low, :presence => true, :numericality => true 
   validates :price_high, :presence => true, :numericality => true
   validates :event_date, :presence => true
+  
+  def event_date_not_from_past
+    if event_date < Date.today
+      errors.add('Data wydarzenia', 'nie może być z przeszłości')
+    end
+  end
 end
