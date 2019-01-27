524f83d9f04ac65714486cd8f9876a8a5e776e7a
basistamarcin <basistamarcin@gmail.com>
2019-01-27 08:12:39 -0500

    Dodana walidacja

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,25 @@ class Event < ApplicationRecord
   validates :price_low, :presence => true, :numericality => true 
   validates :price_high, :presence => true, :numericality => true
   validates :event_date, :presence => true
+  validate :event_date_not_from_past
+  validate :prices_are_greater_than_zero
+  validate :price_high_greater_than_price_low
   
   def event_date_not_from_past
     if event_date < Date.today
       errors.add('Data wydarzenia', 'nie może być z przeszłości')
+    end
+  end
+  
+  def prices_are_greater_than_zero
+    if price_low < 0 or price_high < 0
+      errors.add('Ceny biletów', 'muszą być dodatnie')
+    end
+  end
+  
+  def price_high_greater_than_price_low 
+    if price_low > price_high
+      errors.add('Cena najtańszego biletu', 'nie może być mniejsza od ceny najdroższego')
     end
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,14 @@ class Ticket < ApplicationRecord
   validates :name, :presence => true, :length => { :minimum => 6 }
   validates :email_address, :presence => true 
   validates :price, :presence => true
+  validate :valid_price
+  
+  def valid_price
+    if event.price_low > price
+      errors.add('Cena biletu', 'jest za niska')
+    end
+    if event.price_high < price 
+      errors.add('Cena biletu', 'jest za wysoka')
+    end
+  end
 end
